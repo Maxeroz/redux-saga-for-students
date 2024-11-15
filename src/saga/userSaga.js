2bf19ee06e2969c6332a751f23e5bdf23083d229
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-11-15 20:06:17 +0400

    Redux Saga Task

--- src/saga/userSaga.js
+++ src/saga/userSaga.js
@@ -5,7 +5,12 @@ const fetchUsersFromApi = () =>
     fetch('https://jsonplaceholder.typicode.com/users?_limit=10');
 
 function* fetchUsersWorker() {
-    
+    // Используем эффект 'call' для того чтобы сделать ассинхронную операцию.
+    const data = yield call(fetchUsersFromApi);
+    const json = yield call(() => new Promise((res) => res(data.json())));
+
+    // Используем 'put', это аналог 'dispatch' но в контексте 'SAGA'
+    yield put(setUsers(json));
 }
 
 export function* fetchUsersWatcher() {
